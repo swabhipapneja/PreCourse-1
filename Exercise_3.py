29f8645c8a334c4d083a7a348725a14f38846f05
swabhipapneja <spapneja@gmu.edu>
2024-12-27 00:22:45 -0500

    Singly Linked List Q3 from PC1

--- Exercise_3.py
+++ Exercise_3.py
@@ -2,7 +2,9 @@ class ListNode:
     """
     A node in a singly-linked list.
     """
-    def __init__(self, data=None, next=None):
+    def __init__(self, data, next):
+        self.data = data
+        self.next = None
     
 class SinglyLinkedList:
     def __init__(self):
@@ -12,11 +14,24 @@ class SinglyLinkedList:
         """
         self.head = None
 
+
     def append(self, data):
         """
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        new_node = ListNode(data)
+        if self.head is None:
+            # list is empty
+            self.head = new_node
+            return
+        
+        current = self.head
+        while current.next is not None:
+            current = current.next
+        
+        current.next = new_node
+
         
     def find(self, key):
         """
@@ -24,9 +39,48 @@ class SinglyLinkedList:
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        if self.head is None:
+            # list is empty
+            return
         
+        current = self.head
+        while current is not None:
+            if current.data == key:
+                return current
+            current = current.next
+
+        return None
+        
+    
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        if self.head is None:
+            # list is empty
+            return
+        
+        # if we have to remove the head node
+        if self.head.data == key:
+            self.head = self.head.next
+            return
+
+        current = self.head
+
+        while current.next is not None:
+            if current.next.key == key:
+                current.next = current.next.next
+                return
+            current = current.next
+
+        return
+    
+
+
+
+
+        
+            
+
+
